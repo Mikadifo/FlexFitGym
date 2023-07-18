ae47ca0f13ccba486f0f7d46178f3de7545c0575
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-07-17 21:57:02 -0400

    add: contact and reviews sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import "./App.css";
 import logo from "./assets/logo.png";
 import squareIcon from "./assets/squareIcon.svg";
 import luxuryIcon from "./assets/luxuryIcon.svg";
+import star from "./assets/star.svg";
+import icon from "./assets/icon.png";
+import gymSis from "./assets/gymSis.jpeg";
+import locationIcon from "./assets/locationIcon.svg";
+import "./App.css";
+import "./Navbar.css";
+import "./Hero.css";
+import "./Reviews.css";
+import "./Contact.css";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="body">
-        <div className="hero">
-          <span className="top-hero">
-            <img src={squareIcon} alt="Other brand icon" />
-            TIME FOR FITNESS - 23
-          </span>
-          <h1>OWN YOUR STRENGTH, OWN YOU</h1>
-          <span className="bottom-hero">
-            <a href="#">START YOUR JOURNEY</a>
-            <span>
-              <img src={luxuryIcon} alt="Luxury Icon" />
-              LUXURY FITNESS EXPERIENCE
-            </span>
-          </span>
-        </div>
+        <Hero />
         <div className="image">
           <p>TRAIN ON YOUR OWN TIME</p>
         </div>
-        <div className="contact">three</div>
-        <div className="reviews">four</div>
+        <Contact />
+        <Reviews />
       </div>
     </>
   );
@@ -53,6 +48,74 @@ const Navbar = () => {
       <a href="#" className="join-button">
         JOIN OUR GYM
       </a>
+    </div>
+  );
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <span className="top-hero">
+        <img src={squareIcon} alt="Other brand icon" />
+        TIME FOR FITNESS - 23
+      </span>
+      <h1>OWN YOUR STRENGTH, OWN YOU</h1>
+      <span className="bottom-hero">
+        <a href="#">START YOUR JOURNEY</a>
+        <span>
+          <img src={luxuryIcon} alt="Luxury Icon" />
+          LUXURY FITNESS EXPERIENCE
+        </span>
+      </span>
+    </div>
+  );
+};
+
+const Reviews = () => {
+  return (
+    <div className="reviews">
+      <span className="top-reviews">
+        <span className="score">
+          <h3>4.98</h3>
+          <span>
+            <span className="stars">
+              <img src={star} alt="star1" />
+              <img src={star} alt="star2" />
+              <img src={star} alt="star3" />
+              <img src={star} alt="star4" />
+              <img src={star} alt="star5" />
+            </span>
+            BASED ON 619 REVIEWS
+          </span>
+        </span>
+        <img src={icon} alt="Brand Icon" />
+      </span>
+      <p>
+        "I had been to Al Balad several times. It is one of my favorite places
+        in Jeddah. It is historic, full of great people. I would recommend this
+        place 100%. They have many equipment and the staff is really nice and
+        welcoming."
+      </p>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <span className="contact-box">
+        <img src={gymSis} alt="GymGirl" />
+        <span className="contact-info">
+          <span className="contact-top">
+            24/7 SUPPORT
+            <a href="#" className="address">
+              1234 FITNESS LN BROOKLYN, NY
+              <img src={locationIcon} alt="Location Icon" />
+            </a>
+          </span>
+          <h4>CONTACT US & RISE STRONGER</h4>
+        </span>
+      </span>
     </div>
   );
 };
